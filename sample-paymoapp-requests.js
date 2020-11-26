93137407468f8935d586dc13aa993cc766296d00
Micula C. Timotei <timotei.micula@emmanuel.ro>
2020-11-26 19:42:40 +0200

    tema8

---
+++ sample-paymoapp-requests.js
@@ -0,0 +1,35 @@
+var projects = []
+var projects_id = []
+
+var tasklists = []
+var tasklists_id = []
+
+
+// Get all Projects
+fetch("https://app.paymoapp.com/api/projects/", {
+  headers: {
+    "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
+  },
+})
+.then(res => res.json())
+.then(data => console.log(Object.values(data))
+   
+)
+
+// Get all Tasklists
+fetch("https://app.paymoapp.com/api/tasklists/", {
+  headers: {
+    "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
+  },
+})
+.then(res => res.json())
+.then(data => console.log(data))
+
+// Get all Tasks
+fetch("https://app.paymoapp.com/api/tasks/", {
+  headers: {
+    "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
+  },
+})
+.then(res => res.json())
+.then(data => console.log(data))
